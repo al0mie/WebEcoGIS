3b709852301241191d8b6b0cac15670517f071fb
al0mie <alex.mokrencko@yandex.ru>
2016-04-17 00:40:36 +0300

    include basic arcgis api

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,39 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>EcoGIS</title>
+	<link rel="stylesheet" href="https://js.arcgis.com/4.0beta3/esri/css/main.css">
+	<script src="https://js.arcgis.com/4.0beta3/"></script>
 
-try {
-    $geocode = Geocoder::geocode('10 rue Gambetta, Paris, France');
-    // The GoogleMapsProvider will return a result
-    var_dump($geocode);
-} catch (\Exception $e) {
-    // No exception will be thrown here
-    echo $e->getMessage();
+	<style type="text/css">
+		.esri-logo-svg {
+			display:none;
+		}
 
-}
+	</style>
+</head>
+<body>
+<div id="viewDiv"></div>
+
+<script type="text/javascript">
+
+require([
+  "esri/Map",
+  "esri/views/SceneView",
+  "dojo/domReady!"
+],
+function(Map, SceneView) {
+  var map = new Map({
+    basemap: "streets"
+  });
+  var view = new SceneView({
+    container: "viewDiv",
+    map: map,
+    scale: 50000000,
+    center: [-101.17, 21.78]
+  });
+});
+
+</script>
+</body>
+</html>
