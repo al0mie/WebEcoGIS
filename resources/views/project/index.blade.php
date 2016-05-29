aaee9f1880bbe0d864acdb01f9e4a23112f09bd2
Alex <alex.mokrencko@yandex.ru>
2016-05-30 00:47:21 +0300

    implement view for user projects

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -1,1 +1,32 @@
-sds
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="box-body container">
+        <table class="table table-bordered">
+            <thead>
+            <th>No</th>
+            <th>{{ trans('project.index.name') }}</th>
+            <th class="text-center">{{ trans('project.index.action') }}</th>
+            </thead>
+            <tbody>
+            <?php $no = 1 ?>
+            @foreach ($projects as $project)
+                <tr>
+                    <td>{!! $no !!}</td>
+                    <td>{!! $project->name !!}</td>
+
+                    <td class="text-center">
+                        <a href="{!! route('project.edit', $project->id) !!}">Edit</a>
+                        &middot;
+                    </td>
+                </tr>
+                <?php $no++;?>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+    <div class="text-center">
+
+    </div>
+@stop
