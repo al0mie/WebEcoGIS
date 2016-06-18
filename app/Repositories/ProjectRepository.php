301a0520b9bd659063ff99058527677c765b1f4d
Alex Mokrenko <alex.mokrencko@yandex.ru>
2016-06-18 23:57:18 +0300

    add basic repositories

---
+++ app/Repositories/ProjectRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface ProjectRepository
+ * @package namespace App\Repositories;
+ */
+interface ProjectRepository extends RepositoryInterface
+{
+    //
+}
---
+++ app/Repositories/ProjectRepositoryEloquent.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repositories;
+
+use Prettus\Repository\Eloquent\BaseRepository;
+use Prettus\Repository\Criteria\RequestCriteria;
+use App\Repositories\ProjectRepository;
+use App\Entities\Project;
+use App\Validators\ProjectValidator;
+
+/**
+ * Class ProjectRepositoryEloquent
+ * @package namespace App\Repositories;
+ */
+class ProjectRepositoryEloquent extends BaseRepository implements ProjectRepository
+{
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model()
+    {
+        return Project::class;
+    }
+
+    
+
+    /**
+     * Boot up the repository, pushing criteria
+     */
+    public function boot()
+    {
+        $this->pushCriteria(app(RequestCriteria::class));
+    }
+}
